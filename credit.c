f1bfac990c4596c6eb5349aac367ac7fe18d818b
tigeryu8900 <tigeryu8900@users.noreply.github.com>
2019-09-03 14:11:03 +0000

    automated commit by submit50 [lang=C.UTF-8]

--- credit.c
+++ credit.c
@@ -12,24 +12,27 @@ int main(void)
 {
     long number;
     string type;
-    
+
     // get user input
     number = get_long("Number: ");
-    
+
     // classify number
     long identifier = number;
-    while (identifier > 99) identifier /= 10;
+    while (identifier > 99)
+    {
+        identifier /= 10;
+    }
     switch (identifier / 10)
     {
         case 3:
-	    switch (identifier % 10)
+            switch (identifier % 10)
             {
                 case 4:
                 case 7:
                     type = "AMEX\n";
-                break;
+                    break;
                 default:
-                invalid();
+                    invalid();
             }
             break;
         case 4:
@@ -37,7 +40,7 @@ int main(void)
             break;
         case 5:
             switch (identifier % 10)
-	    {
+            {
                 case 1:
                 case 2:
                 case 3:
@@ -52,7 +55,7 @@ int main(void)
         default:
             invalid();
     }
-    
+
     // verify number
     int checksum = 0;
     long tempNumber = number / 10;
@@ -60,7 +63,7 @@ int main(void)
     {
         int product = 2 * (tempNumber % 10);
         while (product > 0)
-	{
+        {
             checksum += product % 10;
             product /= 10;
         }
@@ -72,10 +75,11 @@ int main(void)
         checksum += tempNumber % 10;
         tempNumber /= 100;
     }
-    if (checksum % 10 != 0 || checksum == 40) {
-	invalid();
+    if (checksum % 10 != 0 || checksum == 40)
+    {
+        invalid();
     }
-    
+
     // print type
     printf("%d%s", checksum, type);
 }
