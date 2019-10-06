e4e3013b57f84ba821ee06848a353342bd36effb
tigeryu8900 <tigeryu8900@users.noreply.github.com>
2019-10-06 12:42:02 +0000

    automated commit by check50 [check50=True]

--- resize.c
+++ resize.c
@@ -73,10 +73,10 @@ int main(int argc, char *argv[])
     // one scanline from inFile.
     RGBTRIPLE *inScanLine = (RGBTRIPLE *)malloc(sizeof(RGBTRIPLE) * oldWidth);
     // 2d array, n scanlines to be written to outfile.
-    RGBTRIPLE **outScanLines = (RGBTRIPLE **)malloc(sizeof(RGBTRIPLE*) * n);
+    RGBTRIPLE **outScanLines = (RGBTRIPLE **)malloc((sizeof(RGBTRIPLE*) + sizeof(RGBTRIPLE) * newWidth) * n);
     for (int i = 0; i < n; ++i) {
         // allocate memory for n scanlines to be written to outfile.
-        outScanLines[i] = (RGBTRIPLE *)malloc(sizeof(RGBTRIPLE) * newWidth);
+        outScanLines[i] = (RGBTRIPLE *)(outScanLines + n);
     }
 
     // filling bytes for padding, 4 bytes max.
@@ -110,9 +110,6 @@ int main(int argc, char *argv[])
 
     // free up allocated memory.
     free(inScanLine);
-    for (int i = 0; i < n; ++i) {
-        free(outScanLines[i]);
-    }
     free(outScanLines);
 
     // close infile
