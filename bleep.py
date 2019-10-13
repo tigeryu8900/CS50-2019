8cf35cdd037ba3f03f27d14e7c5041bf13f60509
tigeryu8900 <tigeryu8900@users.noreply.github.com>
2019-10-13 14:37:52 +0000

    automated commit by check50 [check50=True]

---
+++ bleep.py
@@ -0,0 +1,20 @@
+from cs50 import get_string
+from sys import argv
+import sys
+
+
+def main():
+
+    if len(argv) != 2:
+        sys.exit("Usage: python bleep.py dictionary")
+
+    dictionary = set(open(argv[1]).read().split())
+
+    message = get_string("What message would you like to censor?")
+
+    for word in dictionary:
+        message = message.replace(word, "*" * len(word))
+
+
+if __name__ == "__main__":
+    main()
