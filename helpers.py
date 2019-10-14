a6130a3332bf07392159a4ae036aa35b3896c449
tigeryu8900 <tigeryu8900@users.noreply.github.com>
2019-10-14 11:51:25 +0000

    automated commit by check50 [check50=True]

--- helpers.py
+++ helpers.py
@@ -7,10 +7,10 @@ def lines(a, b):
     a = a.split('\n')
     b = b.split('\n')
     result = set()
-    for i in range(len(a)):
-        for j in range(i + 1, len(b)):
-            if a[i] == b[j]:
-                result.add(a[i])
+    for i in a:
+        for j in b:
+            if a == b:
+                result.add(a)
 
     return list(result)
 
@@ -21,10 +21,10 @@ def sentences(a, b):
     a = sent_tokenize(a)
     b = sent_tokenize(b)
     result = set()
-    for i in range(len(a)):
-        for j in range(i + 1, len(b)):
-            if a[i] == b[j]:
-                result.add(a[i])
+    for i in a:
+        for j in b:
+            if i == j:
+                result.add(i)
 
     return list(result)
 
@@ -33,7 +33,7 @@ def substrings(a, b, n):
     """Return substrings of length n in both a and b"""
 
     result = set()
-    for i in range(len(a) - n):
+    for i in range(len(a) - n + 1):
         if b.find(a[i : i + n]) > -1:
             result.add(a[i : i + n])
 
